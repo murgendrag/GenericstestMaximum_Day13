34a4428fe73a79c9a2587e26e3d9e1d90b14c433
murgendrag <murgendra67@gmail.com>
2022-03-14 23:19:17 +0530

    call in main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GenericstestMaximum_Day13
+{
+    public class Program
+    {
+       public static  void Main(string[] args)
+        {
+            TestNumbers testNumbers = new TestNumbers();
+            testNumbers.findMaximum();
+        }
+    }
+}
